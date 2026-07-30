14c6625df395e85b2d4ba5a99b79a056892d4b0a
Abubakar <cet.abubakar77@gmail.com>
2026-07-30 20:29:24 +0530

    Delete test-ipc.js

--- test-ipc.js
+++
@@ -1,8 +0,0 @@
-const electron = require('electron');
-console.log('Type:', typeof electron);
-console.log('Is function:', typeof electron === 'function');
-console.log('Keys:', Object.keys(electron).slice(0,30).join(', '));
-console.log('Has ipcMain:', 'ipcMain' in electron);
-console.log('Has app:', 'app' in electron);
-console.log('app prop:', typeof Object.getOwnPropertyDescriptor(electron, 'app'));
-setTimeout(() => { require('electron').app.quit(); }, 1000);
