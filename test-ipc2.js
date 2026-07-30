e6f968f349353959740877086af118162d259e9c
Abubakar <cet.abubakar77@gmail.com>
2026-07-30 20:29:32 +0530

    Delete test-ipc2.js

--- test-ipc2.js
+++
@@ -1,5 +0,0 @@
-console.log('process.versions:', JSON.stringify(process.versions));
-console.log('process.argv:', process.argv);
-
-const electron = require('electron');
-console.log('electron type:', typeof electron);
